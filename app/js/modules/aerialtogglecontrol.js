81ae7b91120e7388c64a791b3aeead1b12edef5d
tobinbradley <tobin.bradley@gmail.com>
2017-04-25 15:14:36 -0400

    moved source definition to control

--- app/js/modules/aerialtogglecontrol.js
+++ app/js/modules/aerialtogglecontrol.js
@@ -15,12 +15,22 @@ export default class AerialToggle {
         this._btn.onclick = function() { 
             if (map.getLayer("satellite")) {
                 map.removeLayer("satellite");
+                map.removeSource("satellite");
                 _this._btn.classList.remove('mapboxgl-ctrl-aerial-active');
             } else {
                 map.addLayer({
                     "id": "satellite",
                     "type": "raster",
-                    "source": "satellite",
+                    "source": {
+                        "type": "raster",
+                        "tiles": [
+                            "https://a.tiles.mapbox.com/v4/digitalglobe.nal0g75k/{z}/{x}/{y}.jpg80?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNpbjBkMDhwczA4bWF2cWx1bTIyY2V6aDMifQ.T9gIIn2WngTeTS6J_OU9jw",
+                            "https://b.tiles.mapbox.com/v4/digitalglobe.nal0g75k/{z}/{x}/{y}.jpg80?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNpbjBkMDhwczA4bWF2cWx1bTIyY2V6aDMifQ.T9gIIn2WngTeTS6J_OU9jw",
+                            "https://c.tiles.mapbox.com/v4/digitalglobe.nal0g75k/{z}/{x}/{y}.jpg80?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNpbjBkMDhwczA4bWF2cWx1bTIyY2V6aDMifQ.T9gIIn2WngTeTS6J_OU9jw"
+                        ],
+                        "tileSize": 256,
+                        "maxzoom": 22
+                    },
                     "minzoom": 0,
             		"maxzoom": 22
                 }, _this._placeUnder);
