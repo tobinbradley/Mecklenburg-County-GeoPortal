9328cc09e1dec38c64b82bd8a0fe6ffd713c8ee9
tobinbradley <tobin.bradley@gmail.com>
2017-04-29 11:54:40 -0400

    better photo 404 and single return handling

--- app/js/components/photos.vue
+++ app/js/components/photos.vue
@@ -2,16 +2,16 @@
     <div v-if="results.length > 0" class="mdl-cell mdl-cell--4-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop photos">
         <div class="mdl-card__title photo-big">
             <a v-bind:href="results[photoIndex].photo_url.trim()" target="_blank">
-                <img v-bind:src="results[photoIndex].photo_url.trim()" />
+                <img v-on:error="handleError(photoIndex);" v-bind:src="results[photoIndex].photo_url.trim()" class="mdl-shadow--2dp"/>
             </a>
 
         </div>
         <div class="mdl-card__supporting-text mdl-typography--text-center ">
             {{ results[photoIndex].attribution}} &#8226; {{results[photoIndex].source}}
         </div>
-        <div class="mdl-card__actions mdl-card--border photo-thumb">
+        <div v-if="results.length > 1" class="mdl-card__actions mdl-card--border photo-thumb">
             <img v-for="(item, index) in results" class="mdl-shadow--2dp"
-                v-bind:src="item.photo_url.trim()" v-on:click="handleThumbClick(index)" v-on:error="handleError(index)" />
+                v-on:error="handleError(index);" v-bind:src="item.photo_url.trim()" v-on:click="handleThumbClick(index)"  />
         </div>
     </div>
 </template>
@@ -54,8 +54,8 @@ export default {
         handleThumbClick: function(index) {
             this.photoIndex = index;
         },
-        handleError: function() {
-            this.results.splice(index, 1);
+        handleError: function(index) {
+            this.results.splice(index, 1);            
         }
     }
 
