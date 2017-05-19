7b074508326163b7779b82aec844a0f71e98a842
tobinbradley <tobin.bradley@gmail.com>
2017-05-19 17:51:01 -0400

    asset versioning replaces handlebars

--- build/cachebuster.js
+++ build/cachebuster.js
@@ -1,13 +1,10 @@
-let handlebars = require('handlebars'),
-    fs = require('fs'),
-    path = require('path');
+let Version = require("node-version-assets");
+
+let versionInstance = new Version({
+    assets: ['public/css/main.css', 'public/js/app.js'],
+    grepFiles: ['public/index.html']
+});
+
+versionInstance.run();
 
 
-var source = fs.readFileSync('./app/index.html', 'utf-8').toString();
-var data = {
-    cachebuster: Math.floor((Math.random() * 100000) + 1)
-};
-
-var template = handlebars.compile(source);
-var html = template(data);
-fs.writeFileSync(path.join('./public/', 'index.html'), html);
