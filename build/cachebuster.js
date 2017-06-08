850d76fb93f31318513ad583e877f3ea245577c6
tobinbradley <tobin.bradley@gmail.com>
2017-06-07 20:06:51 -0400

    switched back to asset name versioning

--- build/cachebuster.js
+++ build/cachebuster.js
@@ -1,26 +1,8 @@
-let whiskers = require('whiskers');
-let fs     = require('fs');
-let crypto = require('crypto');
-let path = require('path');
+let Version = require("node-version-assets");
 
-let templateData = {};
+let versionInstance = new Version({
+    assets: ['public/css/main.css', 'public/js/app.js'],
+    grepFiles: ['public/index.html']
+});
 
-// get files
-let html = fs.readFileSync('./app/index.html', 'utf-8').toString();
-let css = fs.readFileSync('./public/css/main.css', 'utf-8');
-let js = fs.readFileSync('./public/js/app.js', 'utf-8');
-
-
-// get hashes
-let md5sum = crypto.createHash('md5');
-let md5sum2 = crypto.createHash('md5');
-md5sum.update(js);
-templateData.js = md5sum.digest('hex');
-md5sum2.update(css);
-templateData.css = md5sum2.digest('hex');
-
-// run template
-let rendered = whiskers.render(html, templateData);
-
-// write file
-fs.writeFileSync(path.join('./public/', 'index.html'), rendered);
+versionInstance.run();
