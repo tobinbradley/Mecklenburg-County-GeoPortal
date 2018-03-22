d9b7e41a551837af3694db53573e5b966a319b54
tobinbradley <tobin.bradley@gmail.com>
2018-03-22 10:59:12 -0400

    added classes and sizes to get IE/Edge working properly

--- build/critical.css.js
+++ build/critical.css.js
@@ -1,12 +1,24 @@
-var critical = require("critical");
+var critical = require('critical');
 
-critical.generate({
-  inline: true,
-  base: "dist/",
-  src: "index.html",
-  dest: "index.html",
-  width: 500,
-  height: 900
-});
-
-console.log("Critical CSS rendered.");
+critical
+  .generate({
+    inline: true,
+    base: 'dist/',
+    src: 'index.html',
+    dest: 'index.html',
+    dimensions: [
+      {
+        height: 200,
+        width: 400
+      },
+      {
+        height: 900,
+        width: 1200
+      }
+    ],
+    minify: true,
+    include: ['#map', '.mapboxgl-missing-css']
+  })
+  .then(function(output) {
+    console.log('Critical CSS rendered.');
+  });
