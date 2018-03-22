2fe0387d1176f7137b13be0b3d305f2a9c123757
tobinbradley <tobin.bradley@gmail.com>
2018-03-22 11:00:33 -0400

    ugly hack to fix gl js IE/Edge rendering bugs

--- app/components/map.js
+++ app/components/map.js
@@ -1,10 +1,10 @@
-import mapboxgl from "mapbox-gl";
-import directions from "../js/directions";
-import fetchNearest from "../js/nearest";
-import { setHash } from "../js/history";
-import AerialToggle from "../js/aerialtogglecontrol";
-import PitchToggle from "../js/pitchtogglecontrol.js";
-import mapStyle from "../style/osm-liberty.json";
+import mapboxgl from 'mapbox-gl';
+import directions from '../js/directions';
+import fetchNearest from '../js/nearest';
+import {setHash} from '../js/history';
+import AerialToggle from '../js/aerialtogglecontrol';
+import PitchToggle from '../js/pitchtogglecontrol.js';
+import mapStyle from '../style/osm-liberty.json';
 
 let template = `
   <div class="map-container card nopadding">
@@ -12,49 +12,62 @@ let template = `
   </div>
 `;
 
+function isIEorEDGE() {
+  return (
+    navigator.appName == 'Netscape' &&
+    (navigator.appVersion.indexOf('Edge') > -1 ||
+      navigator.appVersion.indexOf('Trident') > -1)
+  );
+}
+
 export default {
-  name: "themap",
+  name: 'themap',
   template: template,
   mounted: function() {
     this.initMap();
   },
   watch: {
-    "sharedState.selected.lnglat": "addMarker",
-    "sharedState.show": "mapOverlay",
-    "sharedState.poi": "addPOI"
+    'sharedState.selected.lnglat': 'addMarker',
+    'sharedState.show': 'mapOverlay',
+    'sharedState.poi': 'addPOI'
   },
   methods: {
     initMap: function() {
       let _this = this;
       let mapOptions = {
-        container: "map",
+        container: 'map',
         style: mapStyle,
         attributionControl: false,
         center: [-80.84, 35.26],
         zoom: 8.5,
         minZoom: 8,
-        maxBounds: [[-82.641, 34.115], [-79.008, 36.762]]
-        //preserveDrawingBuffer: true
+        maxBounds: [[-82.641, 34.115], [-79.008, 36.762]],
+        preserveDrawingBuffer: true
       };
 
       _this.privateState.map = new mapboxgl.Map(mapOptions);
       let map = _this.privateState.map;
 
-      // add controls
-      map.addControl(new mapboxgl.FullscreenControl(), "bottom-right");
-      map.addControl(new PitchToggle({}), "bottom-right");
-      map.addControl(new AerialToggle({}), "bottom-right");
+      // hack for IE/Edge not loading map every other load
+      if (isIEorEDGE()) {
+        console.log('Kicking map for Microsoft bugs...');
+        map.setStyle(mapStyle);
+      }
 
-      map.on("load", function() {
+      map.on('load', function() {
         // get map overlays
         _this.mapOverlay();
         // do marker if available
         if (_this.sharedState.selected.lnglat) {
           _this.addMarker();
         }
+        // add controls
+        map.addControl(new mapboxgl.FullscreenControl(), 'bottom-right');
+        map.addControl(new PitchToggle({}), 'bottom-right');
+        map.addControl(new AerialToggle({}), 'bottom-right');
       });
 
-      map.on("click", function(e) {
+      map.on('click', function(e) {
         if (map.getZoom() >= 14 && !_this.privateState.markerClicked) {
           fetchNearest(e.lngLat.lat, e.lngLat.lng, _this.sharedState);
         }
@@ -76,19 +89,19 @@ export default {
       let _this = this;
 
       // remove any overlays
-      if (map.getLayer("overlay")) {
-        map.removeLayer("overlay");
-        map.removeSource("overlay");
+      if (map.getLayer('overlay')) {
+        map.removeLayer('overlay');
+        map.removeSource('overlay');
       }
 
       // add overlays for tabs
       switch (_this.sharedState.show) {
-        case "impervious":
+        case 'impervious':
           map.addLayer({
-            id: "overlay",
-            type: "raster",
+            id: 'overlay',
+            type: 'raster',
             source: {
-              type: "raster",
+              type: 'raster',
               tiles: [
                 `https://mcmap.org/geoserver/postgis/wms?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&width=256&height=256&layers=postgis:impervious_surface&transparent=true`
               ],
@@ -98,17 +111,17 @@ export default {
             minzoom: 15,
             maxzoom: 22,
             paint: {
-              "raster-opacity": 1
+              'raster-opacity': 1
             }
           });
           break;
-        case "environment":
+        case 'environment':
           map.addLayer(
             {
-              id: "overlay",
-              type: "raster",
+              id: 'overlay',
+              type: 'raster',
               source: {
-                type: "raster",
+                type: 'raster',
                 tiles: [
                   `https://mcmap.org/geoserver/postgis/wms?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&width=256&height=256&layers=postgis:view_regulated_floodplains&transparent=true`
                 ],
@@ -118,10 +131,10 @@ export default {
               minzoom: 14,
               maxzoom: 22,
               paint: {
-                "raster-opacity": 1
+                'raster-opacity': 1
               }
             },
-            "waterway_river"
+            'waterway_river'
           );
           break;
       }
@@ -154,8 +167,8 @@ export default {
         )}" target="_blank"  rel="noopener" title="directions">Directions</a>`
       );
       // create DOM element for the marker
-      var el = document.createElement("div");
-      el.classList.add("poiMarker");
+      var el = document.createElement('div');
+      el.classList.add('poiMarker');
       // create the marker
       this.privateState.poiMarker = new mapboxgl.Marker(el, {
         offset: [-20, -20]
@@ -164,8 +177,8 @@ export default {
         .setPopup(popup) // sets a popup on this marker
         .addTo(map);
       document
-        .querySelector(".poiMarker")
-        .addEventListener("click", function(e) {
+        .querySelector('.poiMarker')
+        .addEventListener('click', function(e) {
           _this.privateState.markerClicked = true;
         });
     },
@@ -184,8 +197,8 @@ export default {
         offset: [2, -23]
       }).setHTML(`<strong>${selected.label}</strong><br>${selected.address}`);
       // create DOM element for the marker
-      var el = document.createElement("div");
-      el.classList.add("locationMarker");
+      var el = document.createElement('div');
+      el.classList.add('locationMarker');
       // remove old markers if they exist
       if (this.privateState.locationMarker) {
         this.privateState.locationMarker.remove();
@@ -201,8 +214,8 @@ export default {
         .setPopup(popup) // sets a popup on this marker
         .addTo(map);
       document
-        .querySelector(".locationMarker")
-        .addEventListener("click", function(e) {
+        .querySelector('.locationMarker')
+        .addEventListener('click', function(e) {
           _this.privateState.markerClicked = true;
         });
     }
