ae3a271d31946b9599986e8f501a5a465cce090a
tobinbradley <tobin.bradley@gmail.com>
2018-03-25 15:46:53 -0400

    messed about with preload and tossed sw cache where browsers don't support it

--- build/sw.js
+++ build/sw.js
@@ -4,7 +4,11 @@ const { generateSW } = require("workbox-build");
 generateSW({
   globDirectory: "./dist/",
   globPatterns: ["**/*.{html,js,css,png,jpg,svg,json}"],
-  globIgnores: ["**/service-worker.js"],
+  globIgnores: [
+    "**/service-worker.js",
+    "img/apple-touch-icon-precomposed.png",
+    "img/ms-touch-icon-144x144-precomposed.png"
+  ],
   swDest: "./dist/sw.js",
   dontCacheBustUrlsMatching: /\w{32}\./
 }).then(() => {
