cdae1a4bab428343bc823485c18518a6c46926cc
tobinbradley <tobin.bradley@gmail.com>
2018-04-27 15:31:46 -0400

    switch to free aerials

--- app/js/aerialtogglecontrol.js
+++ app/js/aerialtogglecontrol.js
@@ -1,52 +1,52 @@
 export default class AerialToggle {
-    
-    constructor({placeUnder = 'water_name_line'}) {
-        this._placeUnder = placeUnder;
-    }
+  constructor({ placeUnder = "water_name_line" }) {
+    this._placeUnder = placeUnder;
+  }
 
-    onAdd(map) {
-        this._map = map;
-        let _this = this; 
+  onAdd(map) {
+    this._map = map;
+    let _this = this;
 
-        this._btn = document.createElement('button');
-        this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-aerial';
-        this._btn.type = 'button';
-        this._btn.setAttribute('aria-label', 'toggle satellite imagery');
-        this._btn.onclick = function() { 
-            if (map.getLayer("satellite")) {
-                map.removeLayer("satellite");
-                map.removeSource("satellite");
-                _this._btn.classList.remove('mapboxgl-ctrl-aerial-active');
-            } else {
-                map.addLayer({
-                    "id": "satellite",
-                    "type": "raster",
-                    "source": {
-                        "type": "raster",
-                        "tiles": [
-                            "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZnV6enl0b2xlcmFuY2UiLCJhIjoiWk5SS2NqRSJ9.pt08fCnJBVi8GhH4wYhyiQ"                            
-                        ],
-                        "tileSize": 256,
-                        "maxzoom": 22
-                    },
-                    "minzoom": 0,
-            		"maxzoom": 22
-                }, _this._placeUnder);
-                _this._btn.classList.add('mapboxgl-ctrl-aerial-active');
-            } 
-        };
-        
+    this._btn = document.createElement("button");
+    this._btn.className = "mapboxgl-ctrl-icon mapboxgl-ctrl-aerial";
+    this._btn.type = "button";
+    this._btn.setAttribute("aria-label", "toggle satellite imagery");
+    this._btn.onclick = function() {
+      if (map.getLayer("satellite")) {
+        map.removeLayer("satellite");
+        map.removeSource("satellite");
+        _this._btn.classList.remove("mapboxgl-ctrl-aerial-active");
+      } else {
+        map.addLayer(
+          {
+            id: "satellite",
+            type: "raster",
+            source: {
+              type: "raster",
+              tiles: [
+                "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              ],
+              tileSize: 256,
+              maxzoom: 19
+            },
+            minzoom: 0,
+            maxzoom: 22
+          },
+          _this._placeUnder
+        );
+        _this._btn.classList.add("mapboxgl-ctrl-aerial-active");
+      }
+    };
 
-        this._container = document.createElement('div');
-        this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
-        this._container.appendChild(this._btn);
+    this._container = document.createElement("div");
+    this._container.className = "mapboxgl-ctrl mapboxgl-ctrl-group";
+    this._container.appendChild(this._btn);
 
-        return this._container;
-    }
+    return this._container;
+  }
 
-    onRemove() {
-        this._container.parentNode.removeChild(this._container);
-        this._map = undefined;
-    }
-
+  onRemove() {
+    this._container.parentNode.removeChild(this._container);
+    this._map = undefined;
+  }
 }
