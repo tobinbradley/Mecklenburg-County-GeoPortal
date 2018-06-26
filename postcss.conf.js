84128f4faadad4df6c88ab06c1545444e0429b79
tobinbradley <tobin.bradley@gmail.com>
2018-06-26 16:55:40 -0400

    update dependencies

--- postcss.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  use: ['postcss-import', 'postcss-cssnext', 'cssnano'],
-  'postcss-import': {
-    onImport: function(sources) {
-      global.watchCSS(sources, this.from);
-    }
-  },
-  'postcss-cssnext': {warnForDuplicates: false}
-};
