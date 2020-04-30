be5c39aff333cd12e489d501ae1992a0260067cc
tobinbradley <tobin.bradley@gmail.com>
2020-04-30 16:12:58 -0400

    use purgecss built in to tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,11 @@
 const mode = process.env.NODE_ENV || 'development'
 const production = mode === 'production'
-const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   plugins: [
     require('postcss-import')(),
     require('tailwindcss'),
     require('postcss-preset-env')(),
-    production &&
-      purgecss({
-        content: ['./src/**/*.html', './src/**/*.svelte'],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-        whitelistPatterns: [/mapboxgl/, /svelte-/]
-      }),
     production && require('cssnano')()
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  purge: {
+    mode: 'all',
+    content: ['./src/**/*.html', './src/**/*.svelte', './src/**/*.vue', './src/**/*.jsx'],
+    options: {
+      whitelistPatterns: [/mapboxgl/, /svelte-/]
+    }
+  },
   theme: {
     extend: {
       screens: {
