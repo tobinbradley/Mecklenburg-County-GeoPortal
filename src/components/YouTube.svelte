a996fc94cb6af11654a2a93e7dbb0e1c55f29ef6
tobinbradley <tobin.bradley@gmail.com>
2020-06-22 12:36:56 -0400

    fixed iframe sizes to avoid page jank

--- src/components/YouTube.svelte
+++ src/components/YouTube.svelte
@@ -1,5 +1,6 @@
 <script>
   let tutorialIframe
+
   function loadVideo(vidId) {
     tutorialIframe.setAttribute("src", `https://www.youtube.com/embed/${this.dataset.embed}?autoplay=1`)
     tutorialIframe.classList.remove('hidden')
@@ -7,12 +8,10 @@
 </script>
 
 <style>
-.tutorial-wrapper {
-  padding-bottom: 56.25%;
-}
+
 .play-button {
-    width: 90px;
-    height: 60px;
+  width: 90px;
+  height: 60px;
 }
 .play-button:before {
   content: "";
@@ -30,12 +29,7 @@
 }
 </style>
 
-<div class="max-w-4xl mx-auto mt-16 mb-8 print:hidden">
-  <div class="tutorial-wrapper relative">
-    <div class="youtube w-full flex justify-center items-center absolute top-0 bottom-0">
-      <img src="img/logo.svg" alt="logo image" loading="lazy" class="h-full w-auto">
-      <div class="play-button bg-blue-900 cursor-pointer z-10 rounded-lg shadow-md transition duration-200 ease-in-out hover:bg-red-700 hover:shadow-lg" on:click|once={loadVideo} data-embed="agzk5yeHafg"></div>
-      <iframe bind:this={tutorialIframe} class="hidden w-full h-full absolute t-0 b-0 z-20" frameborder="0" title="video tutorial" />
-    </div>
-  </div>
-</div>
+
+<img src="img/logo.svg" alt="logo image" loading="lazy" class="h-full w-auto">
+<div class="play-button bg-blue-900 cursor-pointer z-10 rounded-lg shadow-md transition duration-200 ease-in-out hover:bg-red-700 hover:shadow-lg" on:click|once={loadVideo} data-embed="agzk5yeHafg"></div>
+<iframe bind:this={tutorialIframe} class="absolute hidden w-full h-full z-20" frameborder="0" title="video tutorial" />
