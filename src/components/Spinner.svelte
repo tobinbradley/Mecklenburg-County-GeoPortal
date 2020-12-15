b88ac68ee01df002dc5d43c9468acd918fff0d31
tobinbradley <tobin.bradley@gmail.com>
2020-12-15 10:44:20 -0500

    add spinner component

---
+++ src/components/Spinner.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let position = ''
+
+</script>
+
+<style>
+/* from https://github.com/loadingio/css-spinner/ */
+.lds-ring {
+  position: absolute;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 34px;
+  height: 34px;
+  border: 6px solid orange;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: orange transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
+
+
+<div class="lds-ring" style="{position}">
+  <div></div><div></div><div></div><div></div>
+</div>
