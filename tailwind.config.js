b30746820ddba1c22e5c64c2dad1ae87f6bc0c2e
tobinbradley <tobin.bradley@gmail.com>
2021-02-05 17:12:01 -0500

    more aggressive CSS purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 module.exports = {
-  // purge: {
-  //   mode: 'all',
-  //   content: ['./src/**/*.svelte', './public/*.html'],
-  //   options: {
-  //     whitelistPatterns: [/mapboxgl/, /svelte-/, /sparkline--/]
-  //   }
-  // },
-  purge: ['./src/**/*.svelte', './public/*.html'],
+  purge: {
+    mode: 'all',
+    content: ['./src/**/*.svelte', './public/*.html'],
+    options: {
+      whitelistPatterns: [/mapboxgl/, /svelte-/, /sparkline--/]
+    }
+  },
   darkMode: false,
   theme: {
     extend: {
       screens: {
-        'print': { 'raw': 'print' },
-        'screen': { 'raw': 'screen' }
+        'print': { 'raw': 'print' }
       }
     }
   },
