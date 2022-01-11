c250710ea4bd4f6d16adef94a4ee40d52a0ec02b
tobinbradley <tobin.bradley@gmail.com>
2022-01-11 17:19:25 -0500

    moved to Vite, some additional tweaks

---
+++ src/main.js
@@ -0,0 +1,37 @@
+// Service worker
+import './js/registerServiceWorker'
+
+// CSS
+import './app.css'
+
+// APP
+import App from './App.svelte'
+import Search from './components/Search.svelte'
+import BackToTop from './components/BackToTop.svelte'
+import YouTube from './components/YouTube.svelte'
+import Toast from './components/Toast.svelte'
+
+// kick off the Svelte
+new Search({
+  target: document.querySelector('#search')
+})
+new YouTube({
+  target: document.querySelector('#tutorial')
+})
+new App({
+  target: document.querySelector('#app')
+})
+new BackToTop({
+  target: document.querySelector(
+    '#backtotop'
+  ),
+  props: {
+    track: document.querySelector(
+      'nav'
+    )
+  }
+})
+new Toast({
+  target: document.querySelector('#toast')
+})
+
---
+++ workbox-config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{css,js,json,ico,html,svg,png,jpg,we}"
+  ],  
+  "swDest": "dist/service-worker.js",
+  "sourcemap": false
+};
