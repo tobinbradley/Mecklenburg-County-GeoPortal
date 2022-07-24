0f7133bac66e2b684589ed9967110751d2e77a85
tobinbradley <tobin.bradley@gmail.com>
2022-07-24 19:39:43 -0400

    set default port with vite 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    port: 3000
+  }
 })
