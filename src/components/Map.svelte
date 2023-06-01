9778f22dda3b8461b1342f6699f5fb21b1cd510d
tobinbradley <tobin.bradley@gmail.com>
2023-06-01 15:16:29 -0400

    pmtiles to mbtiles

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -2,7 +2,6 @@
   import { location } from '../store.js'
   import AerialToggle from '../js/mapcontrolSatellite.js'
   import { createEventDispatcher } from 'svelte'
-  import * as pmtiles from 'pmtiles'
 
   const dispatch = createEventDispatcher()
 
@@ -22,10 +21,6 @@
     (async () => {
       await import("maplibre-gl/dist/maplibre-gl.css")
       const { default: gl } = await import("maplibre-gl")
-
-      //pmtiles
-      let protocol = new pmtiles.Protocol();
-      gl.addProtocol("pmtiles",protocol.tile);
 
       createMap(gl)
     })();
