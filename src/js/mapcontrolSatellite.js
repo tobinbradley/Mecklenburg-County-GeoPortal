bc329d9413c40b69c04858849f41451750d47934
Tobin Bradley <tobin.bradley@gmail.com>
2023-09-08 13:18:16 -0400

    switch to nearmap

--- src/js/mapcontrolSatellite.js
+++ src/js/mapcontrolSatellite.js
@@ -12,13 +12,28 @@ export default class AerialToggle {
     this._btn.type = "button";
     this._btn.setAttribute("aria-label", "toggle satellite imagery");
     this._btn.onclick = function() {
-      if (map.getLayer('satellite').visibility === 'none') {
-        map.setLayoutProperty('satellite', 'visibility', 'visible')
-        _this._btn.classList.add('maplibregl-ctrl-aerial-active')
+      if (map.getLayer('satellite')) {
+        map.removeLayer('satellite')
+        map.removeSource('satellite')
+        _this._btn.classList.remove("maplibregl-ctrl-aerial-active")
       } else {
-        map.setLayoutProperty('satellite', 'visibility', 'none')
-        _this._btn.classList.remove('maplibregl-ctrl-aerial-active')
+        map.addSource("satellite", {
+          type: "raster",
+          tiles: [
+            "https://api.nearmap.com/tiles/v3/Vert/{z}/{x}/{y}.img?apikey=YWI4OGY1YjUtZWNkZC00YzJkLWFjZWUtNmQ1YmExYTRlM2I1"
+          ],
+          tileSize: 256,
+          maxzoom: 19
+        })
+        map.addLayer({
+          id: "satellite",
+          type: "raster",
+          source: "satellite",
+          maxzoom: 22
+        })
+        _this._btn.classList.add("maplibregl-ctrl-aerial-active")
       }
+
     };
 
     this._container = document.createElement("div");
